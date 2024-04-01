736b6dcc0a1edd446d9a05d726fee727f0aef751
Ellen Samanta <ellen.samanta@outlook.com>
2024-04-01 08:02:17 -0300

    feat: migrations criadas

---
+++ Migrations/20240401105647_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace api_usuarios.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "table_register_usuario",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nome = table.Column<string>(type: "text", nullable: false),
+                    profissao = table.Column<string>(type: "text", nullable: false),
+                    departamento = table.Column<string>(type: "text", nullable: false),
+                    cidade = table.Column<string>(type: "text", nullable: false),
+                    data_nascimento = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_table_register_usuario", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "table_register_usuario");
+        }
+    }
+}
