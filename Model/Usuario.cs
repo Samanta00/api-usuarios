c6ffd99bf9bcdf610b9094354c228cf17ea436be
Ellen Samanta <ellen.samanta@outlook.com>
2024-03-31 22:25:48 -0300

    feat: adicionado objetos da model

--- Model/Usuario.cs
+++ Model/Usuario.cs
@@ -1,12 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace api_usuarios.Model
 {
     public class Usuario
     {
-        
+        public int ID { get; set; }
+
+        public string Nome{ get; set; }
+
+        public DateTime DataNascimento { get; set; }
+    
     }
 }
