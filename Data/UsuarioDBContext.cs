55e4ab48a89b99bfefccefb3a0593ad6ca48c100
Ellen Samanta <ellen.samanta@outlook.com>
2024-04-01 08:01:31 -0300

    feat: salvando informações no registro da tabela do banco de dados

--- Data/UsuarioDBContext.cs
+++ Data/UsuarioDBContext.cs
@@ -10,5 +10,21 @@ namespace api_usuarios.Data
         {
         }
         public DbSet<Usuario> usuarios {get; set;}
+
+                protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var usuario = modelBuilder.Entity<Usuario>();
+
+            usuario.ToTable("table_register_usuario");
+            usuario.HasKey(x => x.ID);
+            usuario.Property(x => x.ID).HasColumnName("id").ValueGeneratedOnAdd();
+            usuario.Property(x => x.Nome).HasColumnName("nome").IsRequired();
+            usuario.Property(x => x.Profissao).HasColumnName("profissao");
+            usuario.Property(x => x.Departamento).HasColumnName("departamento");
+            usuario.Property(x => x.Cidade).HasColumnName("cidade");
+            usuario.Property(x => x.DataNascimento).HasColumnName("data_nascimento");
+
+
+        }
     }
 }
