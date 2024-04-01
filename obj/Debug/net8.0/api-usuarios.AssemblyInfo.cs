61343791c5ed34eb891682364e0592c8e35e109e
Ellen Samanta <ellen.samanta@outlook.com>
2024-04-01 10:09:26 -0300

    lint: melhoras executadas

--- obj/Debug/net8.0/api-usuarios.AssemblyInfo.cs
+++ obj/Debug/net8.0/api-usuarios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-usuarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89e7a34231e7bdd245b1d5594df8f7e6f32495b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0846c48e48d8f8189e59a312131712e250eb6d6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-usuarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-usuarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
