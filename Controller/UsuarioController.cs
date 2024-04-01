ba84c4ddeb295917d9bf49d8ab06187f9421d61a
Ellen Samanta <ellen.samanta@outlook.com>
2024-03-31 22:32:19 -0300

    lint: ajustando arquitetura da controller

--- Controller/UsuarioController.cs
+++ Controller/UsuarioController.cs
@@ -1,7 +1,10 @@
-
+using api_usuarios.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace api_usuarios.Controller
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class UsuarioController
     {
         
