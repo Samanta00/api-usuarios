d7ec4e6f2248690920dc01bc3a6f10a093c3cd99
Ellen Samanta <ellen.samanta@outlook.com>
2024-04-01 09:27:00 -0300

    feat: criado função controller para visualizar todos os usuarios registrados.

--- Controller/UsuarioController.cs
+++ Controller/UsuarioController.cs
@@ -1,12 +1,28 @@
 using api_usuarios.Model;
+using api_usuarios.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api_usuarios.Controller
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class UsuarioController
+    public class UsuarioController: ControllerBase
     {
-        
+        private readonly IUsuarioRepository _repository;
+
+        public UsuarioController(IUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var usuariosGet= await _repository.BuscaUsuarios();
+            return usuariosGet.Any()
+            ? Ok(usuariosGet)
+            : NoContent();
+            
+        }
     }
 }
